869e72a0f88771d7022fb7a705b3e4dd2fe48b28
tranphat1357t-commits <tranphat1357t@gmail.com>
2026-05-13 02:38:04 +0700

    Update key.js

--- elite-turbo-mqui.vercel.app/key.js
+++ elite-turbo-mqui.vercel.app/key.js
@@ -2,7 +2,7 @@ window.addEventListener("load", () => {
   (function () {
   "use strict";
 
-  const API_URL = "https://api-server-key.tranphat1357t.workers.dev";
+  const API_URL = "https://serverkeybrian.quannguyendinh508.workers.dev";
 
   // ===== DEVICE ID =====
   function getDeviceId() {
